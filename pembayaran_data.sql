0353a958cc1dcb61fb23add0abbde04ea9fc7a57
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-06 19:15:33 +0700

    Update pembayaran_data.sql

--- pembayaran_data.sql
+++ pembayaran_data.sql
@@ -1,13 +1,13 @@
 INSERT INTO pembayaran (id_tiket, kode_pembayaran, metode_pembayaran)
-VALUES(1115, '384514XY', 'Debit'),
-		(113, '486152AB', 'Tunai'),
-		(1118, '599845CD', 'Debit'),
-		(1112, '684512EF', 'Debit'),
-		(1111, '245812GH', 'Tunai'),
-		(1114, '712354IJ', 'Tunai'),
-		(1116, '928461KL', 'Debit'),
-		(1117, '836475MN', 'Debit'),
-		(1119, '174952OP', 'Tunai'),
-		(1110, '637281QR', 'Tunai');
+VALUES	(1115, '384514XY', 'Debit'),
+	(113, '486152AB', 'Tunai'),
+	(1118, '599845CD', 'Debit'),
+	(1112, '684512EF', 'Debit'),
+	(1111, '245812GH', 'Tunai'),
+	(1114, '712354IJ', 'Tunai'),
+	(1116, '928461KL', 'Debit'),
+	(1117, '836475MN', 'Debit'),
+	(1119, '174952OP', 'Tunai'),
+	(1110, '637281QR', 'Tunai');
 		
 select * from pembayaran;
