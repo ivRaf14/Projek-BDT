20ceb2577a5f1429f8c0603f46472223ce60eb10
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-06 19:15:58 +0700

    Update kelas_data.sql

--- kelas_data.sql
+++ kelas_data.sql
@@ -1,13 +1,13 @@
 INSERT INTO kelas (id_kelas, harga, tipe) 
-VALUES(01, 30000, 'Ekonomi'),
- 		(02, 45000, 'Bisnis'),
-		(03, 60000, 'Eksekutif'),
-		(02, 45000, 'Bisnis'),
-		(03, 60000, 'Eksekutif'),
-		(02, 55000, 'Bisnis'),
-		(01, 40000, 'Ekonomi'),
-		(03, 65000, 'Eksekutif'),
-		(01, 35000, 'Ekonomi'),
-		(02, 50000, 'Bisnis');
+VALUES	(01, 30000, 'Ekonomi'),
+	(02, 45000, 'Bisnis'),
+	(03, 60000, 'Eksekutif'),
+	(02, 45000, 'Bisnis'),
+	(03, 60000, 'Eksekutif'),
+	(02, 55000, 'Bisnis'),
+	(01, 40000, 'Ekonomi'),
+	(03, 65000, 'Eksekutif'),
+	(01, 35000, 'Ekonomi'),
+	(02, 50000, 'Bisnis');
 
 select * from kelas;
