bdd700fb99b35d0840196c69a8a72a841f9f35f9
IvanRafli <67784593+ivRaf14@users.noreply.github.com>
2023-06-06 10:24:50 +0700

    Update kereta.sql

--- kereta.sql
+++ kereta.sql
@@ -1,16 +1,16 @@
-USE base;
+USE data;
 
 CREATE TABLE kereta_api(
-	id_kereta INT NOT NULL primary key,
+	id_kereta VARCHAR(255) NOT NULL primary key,
 	nama VARCHAR(255) NOT NULL,
 	stasiun_keberangkatan VARCHAR(255) NOT NULL,
 	stasiun_tujuan VARCHAR(255) NOT NULL,
-	waktu_keberangkatan INT NOT NULL,
-	waktu_tiba INT NOT NULL	
-);
+	waktu_keberangkatan VARCHAR(255) NOT NULL,
+	waktu_tiba VARCHAR(255) NOT NULL	
+	);
 
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (ARJ1011, 'Arjuna', 'Kota Baru', 'Gubeng', 14:00, 16:30);
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (TUM2012, 'Tumapel', 'Kota Lama', 'Kediri', 14:30, 16:45);
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (PEN1876, 'Penataran', 'Blimbing', 'Kediri', 15:00, 15:50);
+INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES ('ARJ1011', 'Arjuna', 'Kota Baru', 'Gubeng', '14:00', '16:30');
+INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES ('TUM2012', 'Tumapel', 'Kota Lama', 'Kediri', '14:30', '16:45');
+INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES ('PEN1876', 'Penataran', 'Blimbing', 'Kediri', '15:00', '15:50');
 
 select * from kereta_api;
