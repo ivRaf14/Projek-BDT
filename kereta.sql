314ce0d5270c9b572203e6d9fbb6d87b23344f79
IvanRafli <67784593+ivRaf14@users.noreply.github.com>
2023-06-06 10:04:47 +0700

    change time kereta

--- kereta.sql
+++ kereta.sql
@@ -9,8 +9,8 @@ CREATE TABLE kereta_api(
 	waktu_tiba INT NOT NULL	
 );
 
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (ARJ1011, 'Arjuna', 'Kota Baru', 'Gubeng', 1400, 1630);
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (TUM2012, 'Tumapel', 'Kota Lama', 'Kediri', 1430, 1645);
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (PEN1876, 'Penataran', 'Blimbing', 'Kediri', 1500, 1550);
+INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (ARJ1011, 'Arjuna', 'Kota Baru', 'Gubeng', 14:00, 16:30);
+INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (TUM2012, 'Tumapel', 'Kota Lama', 'Kediri', 14:30, 16:45);
+INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES (PEN1876, 'Penataran', 'Blimbing', 'Kediri', 15:00, 15:50);
 
 select * from kereta_api;
