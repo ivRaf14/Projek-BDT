503ac86bdc016f90df8cf8fbcf14f237022c9572
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-06 19:18:59 +0700

    Update pembayaran.sql

--- pembayaran.sql
+++ pembayaran.sql
@@ -6,9 +6,3 @@ CREATE TABLE pembayaran(
 	metode_pembayaran VARCHAR(255) NOT NULL,
      FOREIGN KEY (id_tiket) REFERENCES tiket(id_tiket)
 );
-
-INSERT INTO pembayaran (id_tiket, kode_pembayaran, metode_pembayaran) VALUES (0115, '384514XY', 'Debit');
-INSERT INTO pembayaran (id_tiket, kode_pembayaran, metode_pembayaran) VALUES (0117, '345851XY', 'Debit');
-INSERT INTO pembayaran (id_tiket, kode_pembayaran, metode_pembayaran) VALUES (0114, '385851XY', 'Tunai');
-
-select * from pembayaran;
