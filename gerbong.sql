780454f48d879b808ccf23f6dc3ebd85b8c901dd
IvanRafli <67784593+ivRaf14@users.noreply.github.com>
2023-06-06 14:51:19 +0700

    Update PK

--- gerbong.sql
+++ gerbong.sql
@@ -1,14 +1,13 @@
 USE base;
 
 CREATE TABLE gerbong(
-	id_kereta INT NOT NULL primary key AUTO_INCREMENT,
-	nomor_kursi INT NOT NULL,	
-	nomor_gerbong INT NOT NULL,
-	jumlah_kursi INT NOT NULL
+	id_kereta VARCHAR(255) NOT NULL,
+	nomor_kursi VARCHAR(255) NOT NULL,	
+	nomor_gerbong VARCHAR(255) NOT NULL,
+	jumlah_kursi INT NOT NULL,
+	FOREIGN KEY (id_kereta) REFERENCES kereta_api(id_kereta)
 );
 
-INSERT INTO gerbong (id_kereta, nomor_kursi, nomor_gerbong, jumlah_kursi) VALUES (01, 11, 83851, 50);
-INSERT INTO gerbong (id_kereta, nomor_kursi, nomor_gerbong, jumlah_kursi) VALUES (02, 12, 82808, 50);
-INSERT INTO gerbong (id_kereta, nomor_kursi, nomor_gerbong, jumlah_kursi) VALUES (03, 13, 81503, 50);
+INSERT INTO gerbong (id_kereta, nomor_kursi, nomor_gerbong, jumlah_kursi) VALUES ('ARJ1011', 'C3', 'G1', 50);
 
-select * from gerbong;
+SELECT * from gerbong;
