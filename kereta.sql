32810cd09e1b5a43828ab3b11af985b7da6bac33
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-06 19:18:45 +0700

    Update kereta.sql

--- kereta.sql
+++ kereta.sql
@@ -10,9 +10,3 @@ CREATE TABLE kereta_api(
 	FOREIGN KEY(stasiun_keberangkatan) REFERENCES stasiun(id_stasiun),
     FOREIGN KEY(stasiun_tujuan) REFERENCES stasiun(id_stasiun)
 );
-
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES ('ARJ1011', 'Arjuna', 'KB1896', 'KB1896', '14:00', '16:30');
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES ('TUM2012', 'Tumapel', 'KB1896', 'KB1896', '14:30', '16:45');
-INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) VALUES ('PEN1876', 'Penataran', 'KB1896', 'KB1896', '15:00', '15:50');
-
-select * from kereta_api;
