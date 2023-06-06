3717e0433f00ab9e8e1631f13493568175eca61d
IvanRafli <67784593+ivRaf14@users.noreply.github.com>
2023-06-06 14:53:09 +0700

    Update pembayaran.sql

--- pembayaran.sql
+++ pembayaran.sql
@@ -1,9 +1,10 @@
 USE base;
 
 CREATE TABLE pembayaran(
-	id_tiket INT NOT NULL primary key,
+	id_tiket INT NOT NULL,
 	kode_pembayaran VARCHAR(255) NOT NULL,
-	metode_pembayaran VARCHAR(255) NOT NULL
+	metode_pembayaran VARCHAR(255) NOT NULL,
+     FOREIGN KEY (id_tiket) REFERENCES tiket(id_tiket)
 );
 
 INSERT INTO pembayaran (id_tiket, kode_pembayaran, metode_pembayaran) VALUES (0115, '384514XY', 'Debit');
