f544ee3d921c950755d0411c21b3c58ad7430c07
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-06 10:30:48 +0700

    change data type

--- stasiun.sql
+++ stasiun.sql
@@ -1,13 +1,13 @@
 USE base;
 
 CREATE TABLE stasiun(
-	id_stasiun INT NOT NULL primary key,
+	id_stasiun VARCHAR(255) NOT NULL primary key,
 	nama VARCHAR(255) NOT NULL,
 	alamat VARCHAR(255) NOT NULL
 );
 
-INSERT INTO stasiun (id_stasiun, nama, alamat) VALUES (KB1896, 'Kota Baru', 'Jl. Trunojoyo No.10');
-INSERT INTO stasiun (id_stasiun, nama, alamat) VALUES (GU2975, 'Gubeng', 'Jl. Gubeng Masjid No.1');
-INSERT INTO stasiun (id_stasiun, nama, alamat) VALUES (KE3948, 'Kediri', 'Jl. Stasiun Kediri');
+INSERT INTO stasiun (id_stasiun, nama, alamat) VALUES ('KB1896', 'Kota Baru', 'Jl. Trunojoyo No.10');
+INSERT INTO stasiun (id_stasiun, nama, alamat) VALUES ('GU2975', 'Gubeng', 'Jl. Gubeng Masjid No.1');
+INSERT INTO stasiun (id_stasiun, nama, alamat) VALUES ('KE3948', 'Kediri', 'Jl. Stasiun Kediri');
 
 select * from stasiun;
