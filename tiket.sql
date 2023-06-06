e06ef78f4a3bf0f9f0cc90fb1aba81c4676df320
IvanRafli <67784593+ivRaf14@users.noreply.github.com>
2023-06-06 10:08:46 +0700

    Update tiket.sql

--- tiket.sql
+++ tiket.sql
@@ -1,7 +1,7 @@
 USE base;
 
 CREATE TABLE tiket(
-	id_tiket INT NOT NULL primary key AUTO_INCREMENT,
+	id_tiket INT NOT NULL primary key,
 	id_kelas INT NOT NULL,
 	harga INT NOT NULL,
 	no_kursi INT NOT NULL,
@@ -10,8 +10,8 @@ CREATE TABLE tiket(
 	tujuan VARCHAR(255) NOT NULL
 );
 
-INSERT INTO tiket (id_tiket, id_kelas, harga, no_kursi, waktu_keberangkatan, asal, tujuan) VALUES (0115, 01, 30000, 11, 1400, 'Kota Baru','Gubeng');
-INSERT INTO tiket (id_tiket, id_kelas, harga, no_kursi, waktu_keberangkatan, asal, tujuan) VALUES (0117, 01, 45000, 12, 1430, 'Kota Lama', 'Kediri');
-INSERT INTO tiket (id_tiket, id_kelas, harga, no_kursi, waktu_keberangkatan, asal, tujuan) VALUES (0114, 02, 60000, 13, 1500,'Blimbing', 'Kediri');
+INSERT INTO tiket (id_tiket, id_kelas, harga, no_kursi, waktu_keberangkatan, asal, tujuan) VALUES (0115, 01, 30000, 11, 14:00, 'Kota Baru','Gubeng');
+INSERT INTO tiket (id_tiket, id_kelas, harga, no_kursi, waktu_keberangkatan, asal, tujuan) VALUES (0117, 01, 45000, 12, 14:30, 'Kota Lama', 'Kediri');
+INSERT INTO tiket (id_tiket, id_kelas, harga, no_kursi, waktu_keberangkatan, asal, tujuan) VALUES (0114, 02, 60000, 13, 15:00,'Blimbing', 'Kediri');
 
 select * from tiket;
