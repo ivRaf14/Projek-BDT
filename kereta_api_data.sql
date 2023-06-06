28a534503426e2e733df855b84678005c859ff70
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-06 19:15:45 +0700

    Update kereta_api_data.sql

--- kereta_api_data.sql
+++ kereta_api_data.sql
@@ -1,13 +1,13 @@
 INSERT INTO kereta_api (id_kereta, nama, stasiun_keberangkatan, stasiun_tujuan, waktu_keberangkatan, waktu_tiba) 
-VALUES('ARJ1011', 'Arjuna', 'KB1896', 'KB1896', '14:00', '16:30'),
-		('SRG2022', 'Sriwijaya', 'SG1234', 'SG1234', '09:30', '11:45'),
-		('TGM3033', 'Tawangmangu', 'TM5678', 'TM5678', '11:15', '13:30'),
-		('MPR4044', 'Majapahit', 'MJ9876', 'MJ9876', '16:45', '19:15'),
-		('SMN5055', 'Semeru', 'SM5432', 'SM5432', '13:00', '15:45'),
-		('SLN6066', 'Slamet', 'SL6789', 'SL6789', '07:45', '10:30'),
-		('PRB7077', 'Prambanan', 'PR4321', 'PR4321', '18:30', '20:45'),
-		('BRW8088', 'Bromo', 'BR7654', 'BR7654', '10:00', '12:15'),
-		('TWR9099', 'Tawang', 'TW8765', 'TW8765', '15:30', '18:00'),
-		('MRP0101', 'Merapi', 'MR2109', 'MR2109', '12:45', '15:15');
+VALUES	('ARJ1011', 'Arjuna', 'KB1896', 'KB1896', '14:00', '16:30'),
+	('SRG2022', 'Sriwijaya', 'SG1234', 'SG1234', '09:30', '11:45'),
+	('TGM3033', 'Tawangmangu', 'TM5678', 'TM5678', '11:15', '13:30'),
+	('MPR4044', 'Majapahit', 'MJ9876', 'MJ9876', '16:45', '19:15'),
+	('SMN5055', 'Semeru', 'SM5432', 'SM5432', '13:00', '15:45'),
+	('SLN6066', 'Slamet', 'SL6789', 'SL6789', '07:45', '10:30'),
+	('PRB7077', 'Prambanan', 'PR4321', 'PR4321', '18:30', '20:45'),
+	('BRW8088', 'Bromo', 'BR7654', 'BR7654', '10:00', '12:15'),
+	('TWR9099', 'Tawang', 'TW8765', 'TW8765', '15:30', '18:00'),
+	('MRP0101', 'Merapi', 'MR2109', 'MR2109', '12:45', '15:15');
 		
 select * from kereta_api;
