9e2feb9aea5909af8442fd2050b0a1406849bf54
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-08 18:58:14 +0700

    Update tiket.sql

--- tiket.sql
+++ tiket.sql
@@ -1,14 +1,12 @@
 USE base;
 
-
-
 CREATE TABLE tiket(
 	id_tiket INT NOT NULL primary key,
     id_kereta VARCHAR(255) NOT NULL,
 	id_kelas INT NOT NULL,
     id_penumpang INT NOT NULL,
 	harga INT NOT NULL,
-	no_kursi INT NOT NULL,
+	no_kursi VARCHAR(255) NOT NULL,
     FOREIGN KEY (id_kelas) REFERENCES kelas(id_kelas),
     FOREIGN KEY (id_penumpang) REFERENCES penumpang(id),
     fOREIGN KEY (id_kereta) references kereta_api(id_kereta)
