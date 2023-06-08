9d20fa000b7c29fd9434223b0832e606f5b2b42c
azkannawal <120542108+azkannawal@users.noreply.github.com>
2023-06-08 18:53:32 +0700

    Update gerbong.sql

--- gerbong.sql
+++ gerbong.sql
@@ -1,10 +1,7 @@
 USE base;
 
-
-
 CREATE TABLE gerbong(
 	id_kereta VARCHAR(255) NOT NULL,
-	nomor_kursi VARCHAR(255) NOT NULL,	
 	nomor_gerbong VARCHAR(255) NOT NULL,
 	jumlah_kursi INT NOT NULL,
 	FOREIGN KEY (id_kereta) REFERENCES kereta_api(id_kereta)
